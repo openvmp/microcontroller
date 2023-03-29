22a8bd1fd513b47c521f115cf8277c34d9f9691b
Roman Kuzmenko <clairbee@guidemaze.com>
2023-03-28 18:04:20 -0700

    Fixed build on MacOS and target upload on Ubuntu 22.04 on Raspberry Pi

--- microcontrollers/arduino/shared.mk
+++ microcontrollers/arduino/shared.mk
@@ -2,9 +2,16 @@ OBJDIR = ../../../../build/microcontrollers/arduino
 OBJDIR_CREATE = $(mkdir -p $OBJDIR)
 
 USER_LIB_PATH := ../
-ARDUINO_LIB_PATH = ${HOME}/Arduino/libraries
 ARDUINO_LIBS = arduino
 ARDUINO_LIBS += Servo SoftwareSerial
+
+ifneq ("$(wildcard ${HOME}/Arduino/libraries/Servo)","")
+ARDUINO_LIB_PATH = ${HOME}/Arduino/libraries
+endif
+
+ifneq ("$(wildcard /etc/avrdude.conf)","")
+AVRDUDE_CONF = /etc/avrdude.conf
+endif
 
 CFLAGS = -I$(realpath .) -I../../include
 CXXFLAGS = -I$(realpath .) -I../../include
